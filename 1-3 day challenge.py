a11ead2ecb3f788ad522a94d7715e84f3a945259
shubhiindia789-design <shubhi.india789@gmail.com>
2026-04-03 14:07:27 +0530

    Add files via upload
    This repository contains my daily Python challenge.  Each file shows my progress in learning Python step-by-step.

---
+++ 1-3 day challenge.py
@@ -0,0 +1,123 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("Hello,Shubhi.")
+Hello,Shubhi.
+for i in range(1,101):
+    print(i)
+
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+>>> name = "Shubhi"
+>>> print(name.upper())
+SHUBHI
+>>> print(name.lower())
+shubhi
+>>> print(len(name))
+6
+>>> # checking upper, lower, and lenght in strings
+>>> Line = "Agriculture is thriving in India."
+>>> print(Line.lower())
+agriculture is thriving in india.
+>>> print(Line.upper())
+AGRICULTURE IS THRIVING IN INDIA.
+>>> print(len(Line))
+33
