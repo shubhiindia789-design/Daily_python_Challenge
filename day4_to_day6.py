0a65aece38c6280fca15a68e6a70a42e3ec95272
shubhiindia789-design <shubhi.india789@gmail.com>
2026-04-07 19:29:59 +0530

    Add files via upload

---
+++ day4_to_day6.py
@@ -0,0 +1,12 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> movies = ['Dhurandhar2', '3Idiots']
+>>> print('First movie:', movies[0], 'Second movie:', movies[1])
+First movie: Dhurandhar2 Second movie: 3Idiots
+>>> marks = {'Maths marks':90,'Science marks':95, 'SSt marks':85, 'English marks':98}
+>>> print('English:', marks['English marks'], 'Science:',marks['Science marks'])
+English: 98 Science: 95
+>>> set1 = {'apple', 'mango', 'banana', 'watermelon'}
+>>> set2 = {'watermelon', 'banana'}
+>>> print('Common Fruits:', set1&set2)
+Common Fruits: {'watermelon', 'banana'}
